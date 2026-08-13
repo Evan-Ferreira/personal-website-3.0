aa503ff40d7758cee84660aa736ca86d42aeca48
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-08-13 10:51:23 -0700

    photo: Yosemite #7 - Camp Site 4

--- app/life/photos.ts
+++ app/life/photos.ts
@@ -167,5 +167,13 @@ export const photos: PhotoInput[] = [
             "Nature"
         ],
         "description": "Lands End Lookout #2"
+    },
+    {
+        "file": "life-20260813-lzw4qk.jpg",
+        "tags": [
+            "San Francisco",
+            "Nature"
+        ],
+        "description": "Yosemite #7 - Camp Site 4"
     }
 ];
